04b9de2b474bc5c65eded8b7975a0041b63d0c3f
Gugle <administration@ztxy666.cn>
2025-01-09 05:30:29 +0800

    修改值校验器

--- src/main/java/dev/anvilcraft/rg/api/RGValidator.java
+++ src/main/java/dev/anvilcraft/rg/api/RGValidator.java
@@ -3,24 +3,29 @@ package dev.anvilcraft.rg.api;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
- * RGValidator接口用于定义验证操作的规范
+ * RGValidator 接口用于定义验证操作的规范
  * 它允许实现类针对特定数据类型进行有效性检查
+ *
  * @param <T> 要验证的旧值的类型
  */
 @SuppressWarnings("unused")
 public interface RGValidator<T> {
     /**
      * 验证新值是否有效
+     *
      * @param oldValue 旧值，用于比较或参考
      * @param newValue 新值，需要进行验证
      * @return 如果新值有效则返回true，否则返回false
      */
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     boolean validate(@NotNull T oldValue, @NotNull String newValue);
 
     /**
      * 输入值非法的原因
+     *
      * @return 输入值非法的原因
      */
     default String reason() {
@@ -28,7 +33,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * StringValidator类实现了RGValidator接口，专门用于字符串类型的验证
+     * StringValidator 类实现了 RGValidator 接口，专门用于字符串类型的验证
      */
     class StringValidator implements RGValidator<String> {
         @Override
@@ -42,8 +47,29 @@ public interface RGValidator<T> {
         }
     }
 
+    class StringInSetValidator extends StringValidator {
+        /**
+         * 获取包含在范围内的字符串集合
+         *
+         * @return 包含在范围内的字符串集合
+         */
+        public Set<String> getSet() {
+            return Set.of();
+        }
+
+        @Override
+        public boolean validate(@NotNull String oldValue, @NotNull String newValue) {
+            return super.validate(oldValue, newValue) && getSet().contains(newValue);
+        }
+
+        @Override
+        public String reason() {
+            return "The input value must be in the set: %s!".formatted(getSet().toString());
+        }
+    }
+
     /**
-     * BooleanValidator类实现了RGValidator接口，专门用于布尔类型的验证
+     * BooleanValidator 类实现了 RGValidator 接口，专门用于布尔类型的验证
      */
     class BooleanValidator implements RGValidator<Boolean> {
         @Override
@@ -60,20 +86,33 @@ public interface RGValidator<T> {
     /**
      * NumberValidator抽象类实现了RGValidator接口，为数字类型提供了一个通用的验证框架
      * 它要求子类提供具体的数字解析和范围获取实现
-     * @param <T> 具体数字类型，如Integer、Float等
+     *
+     * @param <T> 具体数字类型，如 Integer、Float 等
      */
-    abstract class NumberValidator<T extends Number> implements RGValidator<Integer> {
+    abstract class NumberValidator<T extends Number> implements RGValidator<T> {
         /**
          * 获取数字的有效范围
+         *
          * @return 包含最小值和最大值的Map.Entry对象
          */
         public abstract @NotNull Map.Entry<T, T> getRange();
 
+        /**
+         * 获取数字是否包含在范围内
+         *
+         * @return 包含范围信息的Map.Entry对象，第一个元素表示最小值是否包含在内，第二个元素表示最大值是否包含在内
+         */
+        public Map.Entry<Boolean, Boolean> containsRange() {
+            return Map.entry(true, true);
+        }
+
         @Override
-        public boolean validate(@NotNull Integer oldValue, @NotNull String newValue) {
+        public boolean validate(@NotNull T oldValue, @NotNull String newValue) {
             try {
                 T value = parse(newValue);
-                return value.doubleValue() >= getRange().getKey().doubleValue() && value.doubleValue() <= getRange().getValue().doubleValue();
+                boolean flag1 = containsRange().getKey() ? value.doubleValue() >= getRange().getKey().doubleValue() : value.doubleValue() > getRange().getKey().doubleValue();
+                boolean flag2 = containsRange().getValue() ? value.doubleValue() <= getRange().getValue().doubleValue() : value.doubleValue() < getRange().getValue().doubleValue();
+                return flag1 && flag2;
             } catch (NumberFormatException e) {
                 return false;
             }
@@ -86,6 +125,7 @@ public interface RGValidator<T> {
 
         /**
          * 解析字符串为指定的数字类型
+         *
          * @param newValue 待解析的字符串
          * @return 解析后的数字
          */
@@ -93,7 +133,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * ByteValidator抽象类继承自NumberValidator，用于byte类型的数字验证
+     * ByteValidator 抽象类继承自 NumberValidator ，用于 byte 类型的数字验证
      */
     abstract class ByteValidator extends NumberValidator<Byte> {
         protected Byte parse(@NotNull String newValue) {
@@ -102,7 +142,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * ShortValidator抽象类继承自NumberValidator，用于short类型的数字验证
+     * ShortValidator 抽象类继承自 NumberValidator ，用于 short 类型的数字验证
      */
     abstract class ShortValidator extends NumberValidator<Short> {
         protected Short parse(@NotNull String newValue) {
@@ -111,7 +151,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * IntegerValidator抽象类继承自NumberValidator，用于int类型的数字验证
+     * IntegerValidator 抽象类继承自 NumberValidator ，用于 int 类型的数字验证
      */
     abstract class IntegerValidator extends NumberValidator<Integer> {
         protected Integer parse(@NotNull String newValue) {
@@ -120,7 +160,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * LongValidator抽象类继承自NumberValidator，用于long类型的数字验证
+     * LongValidator 抽象类继承自 NumberValidator ，用于 long 类型的数字验证
      */
     abstract class LongValidator extends NumberValidator<Long> {
         protected Long parse(@NotNull String newValue) {
@@ -129,7 +169,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * FloatValidator抽象类继承自NumberValidator，用于float类型的数字验证
+     * FloatValidator 抽象类继承自 NumberValidator ，用于 float 类型的数字验证
      */
     abstract class FloatValidator extends NumberValidator<Float> {
         protected Float parse(@NotNull String newValue) {
@@ -138,7 +178,7 @@ public interface RGValidator<T> {
     }
 
     /**
-     * DoubleValidator抽象类继承自NumberValidator，用于double类型的数字验证
+     * DoubleValidator 抽象类继承自 NumberValidator ，用于 double 类型的数字验证
      */
     abstract class DoubleValidator extends NumberValidator<Double> {
         protected Double parse(@NotNull String newValue) {
