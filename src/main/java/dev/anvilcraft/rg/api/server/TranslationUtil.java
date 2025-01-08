742d89a45a5cde59c7a3ba06e0692a39400fe5d8
Gugle <administration@ztxy666.cn>
2025-01-08 22:45:53 +0800

    默认从 en_us 中寻找文本

--- src/main/java/dev/anvilcraft/rg/api/server/TranslationUtil.java
+++ src/main/java/dev/anvilcraft/rg/api/server/TranslationUtil.java
@@ -29,16 +29,20 @@ public class TranslationUtil {
     /**
      * 将密钥转换为相应的文本，并可选择格式替换
      *
-     * @param key 翻译键
+     * @param key  翻译键
      * @param args 可选格式替换参数
      * @return 翻译和格式化的文本
      */
     public static @NotNull MutableComponent trans(String key, Object... args) {
         return Component.translatableWithFallback(
             key,
-            LANGUAGES.getOrDefault(RollingGateServerRules.language, new HashMap<>())
-                .getOrDefault(key, key)
-                .formatted(args),
+            LANGUAGES.getOrDefault(
+                RollingGateServerRules.language,
+                LANGUAGES.getOrDefault(
+                    "en_us",
+                    new HashMap<>()
+                )
+            ).getOrDefault(key, key),
             args
         );
     }
@@ -46,7 +50,7 @@ public class TranslationUtil {
     /**
      * 为指定语言添加一组翻译
      *
-     * @param language 语言代码
+     * @param language     语言代码
      * @param translations 包含翻译的Map
      */
     public static void addLanguage(String language, Map<String, String> translations) {
@@ -58,9 +62,9 @@ public class TranslationUtil {
     /**
      * 从指定的命名空间和语言代码加载语言文件
      *
-     * @param clazz 用于加载资源的类
+     * @param clazz     用于加载资源的类
      * @param namespace 资源命名空间
-     * @param language 语言代码
+     * @param language  语言代码
      */
     @SuppressWarnings("unchecked")
     public static void loadLanguage(Class<?> clazz, String namespace, String language) {
